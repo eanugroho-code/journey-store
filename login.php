d6a682e17f64a150696b71a2d62625ba8331ecaa
JourneyEAN <ekoarinugroho04@gmail.com>
2022-11-01 01:38:54 +0700

    login

--- login.php
+++ login.php
@@ -48,7 +48,7 @@
          }
 
          if (isset($_SESSION['valid'])) {
-            header('Location: https://www.journeyofficial.info/JOURNEYSTORE/');
+            header('Location: https://journeyofficial.info/JOURNEYSTORE/index.php');
          }
       }
    } else {

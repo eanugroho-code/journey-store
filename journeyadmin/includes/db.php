bdf96d82ec0c7947842391c2a5295db5c23c836c
JourneyEAN <ekoarinugroho04@gmail.com>
2022-10-31 18:25:32 +0700

    lh

--- journeyadmin/includes/db.php
+++ journeyadmin/includes/db.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost:3306";
-$user = "cpses_joawe1658r";
+$user = "journeyo_store";
 $pass = "40196Ean!";
 $db = "journeyo_store";
 $conn = mysqli_connect($host, $user, $pass, $db);

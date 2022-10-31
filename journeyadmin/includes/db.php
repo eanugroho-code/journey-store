95becacf460f7b1792cf3093abf5889e9823558f
JourneyEAN <ekoarinugroho04@gmail.com>
2022-10-31 17:44:17 +0700

    host

--- journeyadmin/includes/db.php
+++ journeyadmin/includes/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "cpses_joawe1658r@localhost";
-$pass = "";
+$host = "localhost:3306";
+$user = "cpses_joawe1658r";
+$pass = "40196Ean!";
 $db = "journeyo_store";
 $conn = mysqli_connect($host, $user, $pass, $db);
 //echo 'Connected';
--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost:3306", "cpses_joawe1658r@localhost", "", "journeyo_store");
+$mysqli = new mysqli("localhost:3306", "cpses_joawe1658r", "40196Ean!", "journeyo_store");
 
 // Check connection
 if ($mysqli->connect_errno) {

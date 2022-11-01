3cc2fbbe386c9490c051779ba3626c354c9b598a
JourneyEAN <ekoarinugroho04@gmail.com>
2022-11-01 23:19:43 +0700

    add

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 $id = $_SESSION['id_pelanggan'];
 
 if (!isset($id)) {
-   header('location:https://journeyofficial.info/JOURNEY%20STORE/login.php');
+   header('location:login.php');
 };
 
 if (isset($_GET['logout'])) {

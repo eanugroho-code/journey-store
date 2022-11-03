4d832353a44eab02bd1beacbf26f5c2e7d8aba4f
JourneyEAN <ekoarinugroho04@gmail.com>
2022-11-03 16:07:49 +0700

    db

--- journeyadmin/includes/db.php
+++ journeyadmin/includes/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "journey_store";
+$pass = "40196Ean!";
 $db = "journeyo_store";
 $conn = mysqli_connect($host, $user, $pass, $db);
 //echo 'Connected';
